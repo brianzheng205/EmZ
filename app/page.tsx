1fc07cf5dd71a8f343db9fd068347d4ae0241fae
Brian Zheng <brianzheng205@gmail.com>
2024-12-08 12:38:05 -0500

    Set up TailwindCSS

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import "./globals.css";
 
-export default function Page() {
-  return <h1>Hello, Next.js!</h1>;
+export default function Home() {
+  return (
+    <h1 className="text-3xl font-bold underline text-red-500">Hello world!</h1>
+  );
 }
