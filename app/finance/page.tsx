12f465668af9eeca8a7c83fe5cd87e08cce4bc9d
emilyy-liew <emilyliew2004@gmail.com>
2024-12-27 19:56:32 -0500

    create pretax and posttax categories based on budgets in firestore

--- app/finance/page.tsx
+++ app/finance/page.tsx
@@ -1,3 +1,25 @@
+"use client";
+import { getFirestore, doc, getDoc, DocumentData } from "firebase/firestore";
+import app from "../../firebase/client";
+import { useEffect, useState } from "react";
+
+const fetchData = async () => {
+  const db = getFirestore(app);
+  const docRef = doc(db, "activeBudgets", "2025");
+  const docSnap = await getDoc(docRef);
+  return docSnap.data() as DocumentData;
+};
+
+const fetchBudgets = async (path) => {
+  if (path === undefined || path.length === 0) {
+    return {};
+  }
+  const db = getFirestore(app);
+  const docRef = doc(db, ...path);
+  const docSnap = await getDoc(docRef);
+  return docSnap.data() as DocumentData;
+};
+
 import "../globals.css";
 
 export default function Finance() {
@@ -11,23 +33,6 @@ export default function Finance() {
     "Yearly Z (%)",
     "Yearly Z ($)",
   ];
-  const pretax = new Set([
-    "401k",
-    "HSA",
-    "Insurance",
-    "Commute",
-    "Gross Base",
-    "Gross Stipend",
-    "Gross Bonus",
-  ]);
-  const posttax = new Set([
-    "Roth IRA",
-    "Housing",
-    "Utilities",
-    "Food",
-    "Insurance",
-    "Dates",
-  ]);
 
   function calculateGross(person) {
     let gross = 0;
@@ -43,6 +48,48 @@ export default function Finance() {
     return gross;
   }
 
+  const [emilyBudgetPath, setEmilyBudgetPath] = useState<string[]>([]);
+  const [brianBudgetPath, setBrianBudgetPath] = useState<string[]>([]);
+  const [emilyBudget, setEmilyBudget] = useState<DocumentData>({});
+  const [brianBudget, setBrianBudget] = useState<DocumentData>({});
+
+  useEffect(() => {
+    fetchData().then((document) => {
+      setEmilyBudgetPath(
+        document?.emily?._key.path.segments.slice(
+          document?.emily?._key.path.offset
+        )
+      );
+      setBrianBudgetPath(
+        document?.brian?._key.path.segments.slice(
+          document?.brian?._key.path.offset
+        )
+      );
+    });
+  }, []);
+
+  useEffect(() => {
+    fetchBudgets(emilyBudgetPath).then((document) => {
+      setEmilyBudget(document);
+    });
+  }, [emilyBudgetPath]);
+
+  useEffect(() => {
+    fetchBudgets(brianBudgetPath).then((document) => {
+      setBrianBudget(document);
+    });
+  }, [brianBudgetPath]);
+
+  const posttax = new Set([
+    ...Object.keys(emilyBudget?.postTax || {}),
+    ...Object.keys(brianBudget?.postTax || {}),
+  ]);
+
+  const pretax = new Set([
+    ...Object.keys(emilyBudget?.preTax || {}),
+    ...Object.keys(brianBudget?.preTax || {}),
+  ]);
+
   return (
     <div>
       <table className="border-collapse border border-black">
@@ -51,7 +98,10 @@ export default function Finance() {
             <th></th>
             <th className="border-collapse border border-black">Category</th>
             {headers.map((header, index) => (
-              <th className="border-collapse border border-black" key={index}>
+              <th
+                className="border-collapse border border-black"
+                key={`header-${index}`}
+              >
                 {header}
               </th>
             ))}
@@ -74,8 +124,11 @@ export default function Finance() {
               <td className="border-collapse border border-black">
                 {category}
               </td>
-              {headers.map(() => (
-                <td className="border-collapse border border-black"></td>
+              {headers.map((header, index) => (
+                <td
+                  className="border-collapse border border-black"
+                  key={`post-tax-${category}-${index}`}
+                ></td>
               ))}
             </tr>
           ))}
@@ -95,8 +148,11 @@ export default function Finance() {
               <td className="border-collapse border border-black">
                 {category}
               </td>
-              {headers.map(() => (
-                <td className="border-collapse border border-black"></td>
+              {headers.map((header, index) => (
+                <td
+                  className="border-collapse border border-black"
+                  key={`pre-tax-${category}-${index}`}
+                ></td>
               ))}
             </tr>
           ))}
