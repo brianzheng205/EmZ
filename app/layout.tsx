8502c98e61e65a89fb7a6160170d8a9b0e7a5208
Brian Zheng <brianzheng205@gmail.com>
2024-12-30 00:12:23 -0500

    Add EmZ title

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Link from "next/link";
 import { useState } from "react";
 
 import SideBar, { ToggleSideBar } from "./SideBar";
@@ -16,11 +17,17 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="flex flex-col w-full h-screen bg-background">
-        <div className="flex items-center h-16 bg-primary p-4">
+        <div className="relative flex items-center h-16 bg-primary p-4 text-white">
           <ToggleSideBar
             isOpen={isSideBarOpen}
             onClick={() => setIsSideBarOpen((prev) => !prev)}
           />
+          <Link
+            href="/"
+            className="absolute left-1/2 transform -translate-x-1/2 text-2xl font-bold"
+          >
+            <span>EmZ</span>
+          </Link>
         </div>
 
         <div className="flex flex-grow">
