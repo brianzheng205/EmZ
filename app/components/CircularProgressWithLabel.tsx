a1b18f4ada3a5cf04e069ba81bd8fdcabd94f645
Emily Liew <53126513+emilyy-liew@users.noreply.github.com>
2026-03-21 18:50:36 -0400

    feat: Adjust TV show progress calculation to account for aired episod… (#227)
    * feat: Adjust TV show progress calculation to account for aired episodes and refine percentage display in progress circles based on content status.
    
    * fix: Refine percentage display logic to floor 0% and 100% values based on content status.

--- app/components/CircularProgressWithLabel.tsx
+++ app/components/CircularProgressWithLabel.tsx
@@ -4,8 +4,9 @@ import {
   Box,
   Typography,
 } from "@mui/material";
+import { ContentStatus } from "@shared/tv/types";
 export default function CircularProgressWithLabel(
-  props: CircularProgressProps & { value: number }
+  props: CircularProgressProps & { value: number; status: ContentStatus },
 ) {
   return (
     <Box sx={{ position: "relative", display: "inline-flex" }}>
@@ -26,7 +27,7 @@ export default function CircularProgressWithLabel(
           variant="caption"
           component="div"
           sx={{ color: "text.secondary" }}
-        >{`${Math.floor(props.value)}%`}</Typography>
+        >{`${Math.round(props.value) === 100 && props.status !== ContentStatus.COMPLETED ? Math.floor(props.value) : Math.round(props.value) === 0 && props.status !== ContentStatus.NOT_STARTED ? Math.ceil(props.value) : Math.round(props.value)}%`}</Typography>
       </Box>
     </Box>
   );
